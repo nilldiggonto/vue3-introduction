c8a1a239ea6611848b9787deb8f10d78795ec983
nilldiggonto <nilldiggonto@gmail.com>
2021-05-23 04:51:13 +0600

    Composition API

--- mini-blog/src/App.vue
+++ mini-blog/src/App.vue
@@ -1,9 +1,8 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+ <div>
+  
   <router-view/>
+ </div>
 </template>
 
 <style>
@@ -15,16 +14,4 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- mini-blog/src/views/Home.vue
+++ mini-blog/src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h4>Home</h4>
+    <!-- <p ref="p">The name is {{name}} and age is {{age}}</p>
+    <button @click="handleClick">click here</button>
+    <input type="text" v-model="name"> -->
+    <h2>Refs</h2>
+    <p>{{one.name}} and age is {{one.age}}</p>
+    <button @click="updateOne">Change</button>
+    <hr>
+    <h2>Reactive</h2>
+    <p>{{two.name}} and age is {{two.age}}</p>
+    <button @click="updateTwo">Change</button>
+
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref, reactive } from 'vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  setup() {
+
+    // const p = ref(null)
+    // const name = ref('something')
+    // const age = ref(20)
+    // const handleClick = () =>{
+    //   // console.log('clicked')
+    //   // p.value.classList.add('test')
+    //   // p.value.textContent = 'new refrences'
+    //   name.value = 'changed'
+    // }
+    // return {name,age,handleClick,p}
+    const one = ref({name:'one',age:20})
+    const two = reactive({name:'someone',age:30})
+
+    const updateOne = () =>{
+      one.value.age = 30
+    }
+
+      const updateTwo = () =>{
+      two.age = 15
+    }
+
+    return {one,updateOne,two,updateTwo}
+
+  },
+
+ 
 }
 </script>
