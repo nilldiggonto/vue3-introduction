5531fadf8e6009bc7fe20327d836998a20a19fb4
nilldiggonto <nilldiggonto@gmail.com>
2021-05-23 06:09:39 +0600

    API composition

--- mini-blog/src/views/Home.vue
+++ mini-blog/src/views/Home.vue
@@ -4,13 +4,19 @@
     <!-- <p ref="p">The name is {{name}} and age is {{age}}</p>
     <button @click="handleClick">click here</button>
     <input type="text" v-model="name"> -->
-    <h2>Refs</h2>
+    <!-- <h2>Refs</h2>
     <p>{{one.name}} and age is {{one.age}}</p>
     <button @click="updateOne">Change</button>
     <hr>
     <h2>Reactive</h2>
     <p>{{two.name}} and age is {{two.age}}</p>
-    <button @click="updateTwo">Change</button>
+    <button @click="updateTwo">Change</button> -->
+    <input type="text" v-model="search">
+    <p>{{search}}</p>
+    <hr>
+    <p v-for="name in matchingNames" :key="name">
+      {{name}}
+    </p>
 
 
   </div>
@@ -18,7 +24,7 @@
 
 <script>
 // @ is an alias to /src
-import { ref, reactive } from 'vue'
+import { ref, reactive , computed, watch, watchEffect } from 'vue'
 
 export default {
   name: 'Home',
@@ -34,18 +40,37 @@ export default {
     //   name.value = 'changed'
     // }
     // return {name,age,handleClick,p}
-    const one = ref({name:'one',age:20})
-    const two = reactive({name:'someone',age:30})
 
-    const updateOne = () =>{
-      one.value.age = 30
-    }
 
-      const updateTwo = () =>{
-      two.age = 15
-    }
+    //
+    // const one = ref({name:'one',age:20})
+    // const two = reactive({name:'someone',age:30})
 
-    return {one,updateOne,two,updateTwo}
+    // const updateOne = () =>{
+    //   one.value.age = 30
+    // }
+
+    //   const updateTwo = () =>{
+    //   two.age = 15
+    // }
+
+    const search = ref('')
+    const names = ref(['one','two','three','four','five'])
+
+    watch(search, ()=> {
+      console.log('ok')
+    })
+
+    watchEffect(()=>{
+      console.log(search.value)
+    })
+
+
+    const matchingNames = computed(()=>{
+      return names.value.filter( (name)=> name.includes(search.value))
+    })
+
+    return {names,search,matchingNames}
 
   },
 
