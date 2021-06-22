b6ea8e48f33c700b1056b387b351797aa1904123
nilldiggonto <nilldiggonto@gmail.com>
2021-06-23 05:26:19 +0600

    Async js

--- async_javascript/main_fetch.js
+++ async_javascript/main_fetch.js
@@ -1,9 +1,23 @@
-fetch('test.json').then( (response)=>{
-    console.log(response);
-    return response.json();
+// fetch('test.json').then( (response)=>{
+//     console.log(response);
+//     return response.json();
     
-}).then(data =>{
-    console.log(data);
-}).catch((err)=>{
-    console.log(err);
-});
+// }).then(data =>{
+//     console.log(data);
+// }).catch((err)=>{
+//     console.log(err);
+// });
+
+const getDatas = async ()=>{
+    const response = await fetch('test.json');
+    if (response.status !== 200){
+        throw new Error('something wrong with server');
+    }
+    const data = await response.json()
+    // console.log(data)
+    return data;
+};
+
+getDatas()
+    .then( data=>console.log(data))
+    .catch(err=>console.log(err.message));
